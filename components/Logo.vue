0d161772662c51f61e4fd790e359ba77c2be8068
summeryio <summeryio@summeryiodeiMac.local>
2020-07-07 22:12:21 +0800

    init

---
+++ components/Logo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="logo">
+    <a href="/">logo</a>
+  </div>
+  
+</template>
+
+<style lang="scss">
+.logo {
+  a {
+    font-size: 30px;
+  }
+}
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
